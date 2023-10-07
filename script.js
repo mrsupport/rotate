ab068f0f68a10db56b337efa9c30da53edd18dd0
Xin Musa <144815263+mrsupport@users.noreply.github.com>
2023-10-07 13:06:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.getElementById('rotateIpButton').addEventListener('click', () => {
+document.getElementById('rotate-ip-button').addEventListener('click', () => {
+    // Make a POST request to your Heroku server's endpoint
     fetch('https://rotatepk-a750b8a2f3fb.herokuapp.com/rotate-ip', {
         method: 'POST',
     })
     .then(response => {
         if (response.ok) {
-            console.log('IP rotation successful');
+            console.log('ADB commands triggered successfully');
         } else {
-            console.error('IP rotation failed');
+            console.error('Failed to trigger ADB commands');
         }
     })
     .catch(error => {
