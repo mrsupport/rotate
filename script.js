4bbd3f4c33578e1ed23b4342e8583ad52b631560
Xin Musa <144815263+mrsupport@users.noreply.github.com>
2023-10-07 13:13:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-document.getElementById('rotate-ip-button').addEventListener('click', () => {
-    // Make a POST request to your Heroku server's endpoint
-    fetch('https://rotatepk-a750b8a2f3fb.herokuapp.com/rotate-ip', {
-        method: 'POST',
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log('ADB commands triggered successfully');
-        } else {
-            console.error('Failed to trigger ADB commands');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    // Wait for the DOM to be fully loaded before attaching the event listener
+
+    // Find the "Rotate IP" button by its ID
+    const rotateIpButton = document.getElementById("rotate-ip-button");
+
+    if (rotateIpButton) {
+        // If the button exists, attach a click event listener
+        rotateIpButton.addEventListener("click", function () {
+            // Make a POST request to your Heroku server's endpoint
+            fetch("https://rotatepk-a750b8a2f3fb.herokuapp.com/rotate-ip", {
+                method: "POST",
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        console.log("ADB commands triggered successfully");
+                    } else {
+                        console.error("Failed to trigger ADB commands");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        });
+    } else {
+        console.error('Button with id "rotate-ip-button" not found');
+    }
 });
